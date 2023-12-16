224b8b641ee5a15bd4b97c47f8b3ad52884da6a2
Bahriddin <bahriddinboltayev75@gmail.com>
2023-12-16 19:54:20 +0500

    first commot

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://itransition-intern-task4.onrender.com",
+      "https://itransition-task4-intern.netlify.app/",
     ],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
