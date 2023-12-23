0c9f5c1a94cb049ab83ccfe8d7a97599ffc9c6ad
Bahriddin Boltayev <92244710+Bahriddin0707@users.noreply.github.com>
2023-12-23 12:55:46 +0500

    Update index.js

--- index.js
+++ index.js
@@ -12,10 +12,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://itransition-task4-intern.netlify.app",
-    ],
+    origin:"*",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
